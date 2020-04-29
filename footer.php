6e04765036f6538def132a6827ff3b0d73093d68
Nikolas Haug <nikohaug21@gmail.com>
2020-04-29 11:52:59 -0500

    fix container padding styles in layout

--- footer.php
+++ footer.php
@@ -18,18 +18,18 @@
                     </div>
                 </div>  
                 <div class="row">
-                <div class="col text-center">
-                    <?php if(get_theme_mod( 'icon1_icon', '' )) : ?>
-                        <a href="<?php echo get_theme_mod( 'icon1_url', '' ); ?>" target="_blank" rel="noopener"><span class="social-icon <?php echo get_theme_mod( 'icon1_icon', '' ); ?>"></span></a>
-                    <?php endif; ?>
-                    <?php if(get_theme_mod( 'icon2_icon', '' )) : ?>
-                        <a href="<?php echo get_theme_mod( 'icon2_url', '' ); ?>" target="_blank" rel="noopener"><span class="social-icon <?php echo get_theme_mod( 'icon2_icon', '' ); ?>"></span></a>
-                    <?php endif; ?>
-                    <?php if(get_theme_mod( 'icon3_icon', '' )) : ?>
-                        <a href="<?php echo get_theme_mod( 'icon3_url', '' ); ?>" target="_blank" rel="noopener"><span class="social-icon <?php echo get_theme_mod( 'icon3_icon', '' ); ?>"></span></a>
-                    <?php endif; ?>
-                    <p><?php bloginfo('name'); ?> &copy; <?php echo date('Y'); ?></p>
-                </div>
+                    <div class="col text-center">
+                        <?php if(get_theme_mod( 'icon1_icon', '' )) : ?>
+                            <a href="<?php echo get_theme_mod( 'icon1_url', '' ); ?>" target="_blank" rel="noopener"><span class="social-icon <?php echo get_theme_mod( 'icon1_icon', '' ); ?>"></span></a>
+                        <?php endif; ?>
+                        <?php if(get_theme_mod( 'icon2_icon', '' )) : ?>
+                            <a href="<?php echo get_theme_mod( 'icon2_url', '' ); ?>" target="_blank" rel="noopener"><span class="social-icon <?php echo get_theme_mod( 'icon2_icon', '' ); ?>"></span></a>
+                        <?php endif; ?>
+                        <?php if(get_theme_mod( 'icon3_icon', '' )) : ?>
+                            <a href="<?php echo get_theme_mod( 'icon3_url', '' ); ?>" target="_blank" rel="noopener"><span class="social-icon <?php echo get_theme_mod( 'icon3_icon', '' ); ?>"></span></a>
+                        <?php endif; ?>
+                        <p><?php bloginfo('name'); ?> &copy; <?php echo date('Y'); ?></p>
+                    </div>
                 </div>              
             </div>
         </footer>
